55dd6d11014fce2d8e471f3245394093fac0eaf7
memakergytcom <me@makergyt.com>
2020-03-29 20:17:36 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# hexo-tag-book-douban
+[![hexo-tag-book-douban](https://badgen.net/npm/v/hexo-tag-book-douban)](https://www.npmjs.com/package/hexo-tag-book-douban)
+![](https://img.shields.io/badge/Generator-Hexo-0e83cd?&logo=hexo)
+![license](https://badgen.net/github/license/makergyt/hexo-tag-book-douban)
+![last-commit](https://badgen.net/github/last-commit/makergyt/hexo-tag-book-douban)
+
+A tag to display the information of Douban books in hexo post/page.
+
+## Feature
+- Rendering book information according to book ID
+  - id,
+  - title
+  - alt
+  - image
+  - publisher
+  - pubdate
+  - author
+  - translator
+  - price
+  - rating
+  - summary
+
+## Dependencies
+- [axios](https://github.com/axios/axios) => GET book data
+- [Douban API Proxy](https://douban.uieee.com/) => A proxy service of Douban API
+
+## Install
+```sh
+npm i hexo-tag-book-douban --save
+# Hexo/source/_posts/xxx.md
+{% dbook [id] %}
+```
+## Development
+```sh
+git clone https://github.com/MakerGYT/hexo-tag-book-douban.git
+```
+If secondary development, please fork.
+## Bug
+If there is a bug, please feedback through [issues](https://github.com/MakerGYT/hexo-tag-book-douban/issues)
+
+## Connect
+If you have any questions, please contact me at the following contact information.
+
+- Email: me@makergyt.com
+
+## Todo
+- [ ] Support for custom styles and templates
+
+## License
+MIT License
+
+## Reference
+- [hexo-tag-douban](https://github.com/YuyingWu/hexo-tag-douban): Didn't work because official interface requires certification.
---
+++ index.js
@@ -0,0 +1,60 @@
+/**
+* hexo-tag-book-douban
+* https://github.com/MakerGYT/hexo-tag-book-douban
+* Copyright (c) 2020, MakerGYT
+* Licensed under the MIT license.
+* Syntax:
+* {% dbook [id] %}
+*/
+
+const axios = require('axios');
+
+const getBook = async (id) => {
+  let fields = 'id,title,alt,image,publisher,pubdate,author,translator,price,rating,summary,isbn13';
+  return  await axios(
+    {
+      url: `https://douban.uieee.com/v2/book/${id}?fields=${fields}`,
+      method: 'get',
+    });
+}
+hexo.extend.tag.register('dbook', (args) => {
+  let tpl;
+  
+  return getBook(args[0]).then((res) => {
+
+    if (200 === res.status) {
+      let { title,alt,image,publisher,pubdate,author,translator,price,rating,summary,isbn13 } = res.data.data;
+      tpl = [
+        '<a title="'+ summary +'" href="' + alt + '" target="_blank" style="color: #666;display:block;">',
+          '<img src="' + image + '" alt="' + title + '" style="width: 135px; height: 192px; display:inline-block; margin-right: 14px;"  referrerpolicy="no-referrer" />',
+          '<div class="info" style="display:inline-block; vertical-align: top;">',
+            '<p style="margin:0; font-size: 16px; font-weight: bold;">' + title + '</p>',
+            '<p style="margin:0; font-size: 14px;">作者：' + author.join(',') + '</p>',
+            '<p style="margin:0; font-size: 14px;">出版社：' + publisher + '</p>',
+            '<p style="margin:0; font-size: 14px;">出版日期：' + pubdate + '</p>',
+            translator.length ? '<p style="margin:0; font-size: 14px;">译者：' + translator.join(',') + '</p>' : '',
+            '<p style="margin:0; font-size: 14px;">定价：' + price + ' 评分：' + rating.average + '</p>',
+            '<p style="margin:0; font-size: 14px;">ISBN：' + isbn13 + '</p>',
+          '</div>',
+        '</a>'
+      ].join('');
+    } else {
+      tpl = JSON.stringify(res);
+    }
+    return '<div class="hexo-tag-book-douban" style="position:relative;width:60%;border-radius: 4px;box-shadow: 0 0 10px #ccc;margin:10px auto;padding: 10px;">'
+      + tpl
+      + '<a href="https://github.com/makergyt/hexo-tag-book-douban" title="hexo-tag-book-douban" target="_blank" style="display:block;width:24px;height:24px;position: absolute; bottom:10px;right: 39px;"><img src="https://github.com/fluidicon.png" alt="" style="display:block;width:24px;height:24px;" /></a>'
+      + '</div>';
+  }).catch((err)=> {
+    console.error(err)
+    if(404 === err.response.status) {
+      tpl = err.response.data.msg;
+    } else {
+      tpl = 'Interface call failed, possibly due to current restriction'
+    }
+    return '<div class="hexo-tag-book-douban" style="position:relative;width:60%;border: 1px solid red;border-radius: 4px;box-shadow: 0 0 10px #ccc;margin:10px auto;padding: 10px;">'
+      + tpl
+      + '<a href="https://github.com/makergyt/hexo-tag-book-douban" title="hexo-tag-book-douban" target="_blank" style="display:block;width:24px;height:24px;position: absolute; bottom:10px;right: 39px;"><img src="https://github.com/fluidicon.png" alt="" style="display:block;width:24px;height:24px;" /></a>'
+      + '</div>';
+  });
+}, { async: true });
